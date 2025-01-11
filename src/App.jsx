d2103254fbfee7fc2140bad5b2a9a6e6228664a9
cristiansot <soluciongrafix@gmail.com>
2025-01-11 12:29:22 -0300

    Se agrega componente testimonios

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import AppointmentForm from "./components/AppointmentForm";
 import ServiceList from "./components/ServiceList";  
 import FiltroEspecialidad from "./components/FiltroEspecialidad";  
 import CarouselExample from "./components/Carousel";
+import Testimonios from "./components/Testimonios";
 import "bootstrap/dist/css/bootstrap.min.css";
 import './App.css';
 
@@ -69,8 +70,13 @@ const App = () => {
           />
           <CarouselExample />
           <p style={{ marginTop: 30, fontSize: 18 }}>
-            Nursing Hospital es la red de atención médica más importante del país...
+            Nursing Hospital es la red de atención médica más importante del país, reconocida por su compromiso con la excelencia en salud y la formación integral de futuros profesionales. Con un vasto campo clínico que abarca especialidades y niveles de atención de alta complejidad, esta institución no solo se dedica a ofrecer cuidados de calidad, sino también a liderar programas de capacitación avanzada para los médicos del mañana. Desde sus modernos hospitales hasta sus clínicas y centros de especialidades, Nursing Hospital fomenta la innovación, el aprendizaje práctico y una atención humanizada que impacta positivamente en la salud de miles de personas en todo el país.<br />
+            <br />
+            Nuestra Misión es entregar a la persona y a su familia una Atención de Salud Integral y de calidad que contribuya a su bienestar, respetando su dignidad, por equipos de gran calidad humana y excelencia profesional y académica.<br />
+            <br />
+            Nuestra Visión es ser la Red de Salud líder en Latinoamérica en la práctica clínica de excelencia y en el desarrollo de conocimientos para el cuidado de la persona, en colaboración con otras instituciones nacionales y extranjeras.
           </p>
+          <Testimonios />
         </>
       )}
 
